070d3ff0f8217f4fcfee9fe207dff046d5edc476
Krishna Shrivastava <krshnshrvstv.gwl@gmail.com>
2024-11-22 18:20:47 +0530

    changes

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-export const PORT = 5555;
+export const PORT = process.env.PORT ||5555;
 
 export const mongoURL = 'mongodb+srv://per550017:UK4sMdjTnbTugzL9@book-store-mern-stack.c5fbo.mongodb.net/?retryWrites=true&w=majority&appName=Book-store-Mern-Stack'
